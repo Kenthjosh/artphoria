3474d428e8847935f48f157f16ccc46185268b0b
Kenthjosh <oyaokenthjosh@gmail.com>
2025-01-10 23:40:26 +0800

    First Commit

---
+++ index.php
@@ -0,0 +1,369 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <!-- meta data -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!--font-family-->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&amp;subset=devanagari,latin-ext" rel="stylesheet">
+
+    <!-- title of site -->
+    <title>Artphoria</title>
+
+    <!-- For favicon png -->
+    <link rel="shortcut icon" type="image/icon" href="assets/logo/logo2.png"/>
+
+    <!--font-awesome.min.css-->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+    <!--flat icon css-->
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+
+    <!--animate.css-->
+    <link rel="stylesheet" href="assets/css/animate.css">
+
+    <!--owl.carousel.css-->
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+
+    <!--bootstrap.min.css-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <!-- bootsnav -->
+    <link rel="stylesheet" href="assets/css/bootsnav.css" >
+
+    <!--style.css-->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!--responsive.css-->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+
+    <!-- [if lt IE 9]> -->
+        <!-- <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script> -->
+    <!-- <![endif] -->
+
+</head>
+<body>
+    <!-- top-area Start -->
+    <header class="top-area">
+        <div class="header-area">
+            <!-- Start Navigation -->
+            <nav class="navbar navbar-default bootsnav navbar-fixed dark no-background">
+                <div class="container">
+                    <!-- Start Header Navigation -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
+                            <i class="fa fa-bars"></i>
+                        </button>
+                        <a class="navbar-brand" href="#welcome-hero">Artphoria</a>
+                    </div><!--/.navbar-header-->
+                    <!-- End Header Navigation -->
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse menu-ui-design" id="navbar-menu">
+                        <ul class="nav navbar-nav navbar-right" data-in="fadeInDown" data-out="fadeOutUp">
+                        <li class=" smooth-menu active"></li>
+                            <li class=" smooth-menu"><a href="#welcome-hero">home</a></li>
+                            <li class=" smooth-menu"><a href="#about">about</a></li>
+                            <li class="smooth-menu"><a href="#artworks">artworks</a></li>
+                        </ul><!--/.nav -->
+                    </div><!-- /.navbar-collapse -->
+                </div><!--/.container-->
+            </nav><!--/nav-->
+            <!-- End Navigation -->
+        </div><!--/.header-area-->
+
+        <div class="clearfix"></div>
+
+    </header><!-- /.top-area-->
+    <!-- top-area End -->
+
+    <!--welcome-hero start -->
+    <section id="welcome-hero" class="welcome-hero">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="header-text">
+                        <h2>hi <span>,</span> welcome to<br>Artphoria<span>.</span>   </h2>
+                        <p>My collection of cartoon arts</p>
+                    </div><!--/.header-text-->
+                </div><!--/.col-->
+            </div><!-- /.row-->
+        </div><!-- /.container-->
+
+    </section><!--/.welcome-hero-->
+    <!--welcome-hero end -->
+
+    <!--about start -->
+    <section id="about" class="about">
+        <div class="section-heading text-center">
+            <h2>about me</h2>
+        </div>
+        <div class="container">
+            <div class="about-content">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <!-- About me text here... -->
+                        <div class="single-about-txt">
+                            <h3 style="text-align:justify;">
+                                Hi, I’m Remelyn Mongaya Estanque, a third-year BSIT student. I'm really passionate about both technology and creativity, which is why I love being a part of "The Modern Fisher," our campus organization. I’m also the Cartoon Editor, where I get to use my artistic side to bring fun and engaging stories to life. Balancing school and being involved in campus activities is important to me, and I’m always looking for ways to grow and make a positive impact on my college community.
+                            </h3>
+                            <p style="text-align:justify;">
+                                Ever since I was a child, my passion for drawing has been deeply rooted in my love for anime. Watching captivating stories filled with vibrant characters inspired me to pick up a pencil and bring my imagination to life. I spent countless hours in my room sketching my favorite characters, immersing myself in the creative process. Over time, I began to recognize my potential, and my determination to improve led me to participate in various art competitions. Winning many of these contests from elementary through senior high school fueled my passion even more and solidified my dream of sharing stories through art.
+                            </p>
+                            <p style="text-align:justify;">
+                                Now, as a third-year college student, I have come a long way from the kid who spent days perfecting anime sketches. When I first heard about the screening for The Modern Fisher, our campus publication, I immediately saw it as a chance to turn my passion into a meaningful contribution. I was thrilled to be accepted as a cartoonist and proudly served as Chief Cartoonist during my first and second years. Today, that same kid who once drew anime characters is the Cartoon Editor of The Modern Fisher, where I contribute engaging, creative stories that resonate with our readers.
+                            </p>
+                            <div class="row">
+                                <!-- Additional info -->
+                                <div class="col-sm-4">
+                                    <div class="single-about-add-info">
+                                        <h3>phone</h3>
+                                        <p>(+63) 930 446 6778</p>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4">
+                                    <div class="single-about-add-info">
+                                        <h3>email</h3>
+                                        <p>estanqueremelyn997@gmail.com</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-offset-1 col-sm-5">
+                        <!-- Image and links  -->
+                        <div class="single-about-img">
+                            <img src="assets/images/about/profile/profile-image.jpg" alt="profile_image" >
+                            <div class="about-list-icon">
+                                <ul>
+                                    <li>
+                                        <a href="https://www.facebook.com/share/18EX2CiRyx/">
+                                            <i  class="fa fa-facebook" aria-hidden="true"></i>
+                                        </a>
+                                    </li><!-- / li -->
+                                    <li>
+                                        <a href="https://www.instagram.com/itzzz_nyle/?igsh=MnZ6Z3RxN3hrOGlz&fbclid=IwZXh0bgNhZW0CMTEAAR1hh2VovTYrLH053A5fjELugaGG5V3leGNvcrE2WlXJCs_CRLcW3ZCU5E4_aem_b4KgAxalAPaoKhoEzTMlTA">
+                                            <i  class="fa fa-instagram" aria-hidden="true"></i>
+                                        </a>
+                                    </li><!-- / li -->
+                                </ul><!-- / ul -->
+                            </div><!-- /.about-list-icon -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section><!--/.about-->
+    <!--about end -->
+
+    <!--artworks start -->
+    <section id="artworks" class="portfolio">
+        <div class="portfolio-details">
+            <div class="section-heading text-center">
+                <h2>artworks</h2>
+            </div>
+            <div class="container">
+                <div class="portfolio-content">
+                    <div class="isotope">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <div class="item">
+                                    <img src="assets/images/creations/1.png" alt="artwork image"/>
+                                    <div class="isotope-overlay">
+                                        <a href="#">
+                                            Artwork #1
+                                        </a>
+                                    </div><!-- /.isotope-overlay -->
+                                </div><!-- /.item -->
+                                <div class="item">
+                                    <img src="assets/images/creations/2.png" alt="artwork image"/>
+                                    <div class="isotope-overlay">
+                                        <a href="#">
+                                            Artwork #2
+                                        </a>
+                                    </div><!-- /.isotope-overlay -->
+                                </div><!-- /.item -->
+                                <div class="item">
+                                    <img src="assets/images/creations/3.png" alt="artwork image"/>
+                                    <div class="isotope-overlay">
+                                        <a href="#">
+                                            Artwork #3
+                                        </a>
+                                    </div><!-- /.isotope-overlay -->
+                                </div><!-- /.item -->
+                                <div class="item">
+                                    <img src="assets/images/creations/4.png" alt="artwork image"/>
+                                    <div class="isotope-overlay">
+                                        <a href="#">
+                                            Artwork #4
+                                        </a>
+                                    </div><!-- /.isotope-overlay -->
+                                </div><!-- /.item -->
+                                <div class="item">
+                                    <img src="assets/images/creations/5.png" alt="artwork image"/>
+                                    <div class="isotope-overlay">
+                                        <a href="#">
+                                            Artwork #5
+                                        </a>
+                                    </div><!-- /.isotope-overlay -->
+                                </div><!-- /.item -->
+                            </div><!-- /.col -->
+                            <div class="col-sm-4">
+                                <div class="item">
+                                    <img src="assets/images/creations/6.png" alt="artwork image"/>
+                                    <div class="isotope-overlay">
+                                        <a href="#">
+                                            Artwork #6
+                                        </a>
+                                    </div><!-- /.isotope-overlay -->
+                                </div><!-- /.item -->
+                                <div class="item">
+                                    <img src="assets/images/creations/7.png" alt="artwork image"/>
+                                    <div class="isotope-overlay">
+                                        <a href="#">
+                                            Artwork #7
+                                        </a>
+                                    </div><!-- /.isotope-overlay -->
+                                </div><!-- /.item -->
+                                <div class="item">
+                                    <img src="assets/images/creations/8.png" alt="artwork image"/>
+                                    <div class="isotope-overlay">
+                                        <a href="#">
+                                            Artwork #8
+                                        </a>
+                                    </div><!-- /.isotope-overlay -->
+                                </div><!-- /.item -->
+                                <div class="item">
+                                    <img src="assets/images/creations/9.png" alt="artwork image"/>
+                                    <div class="isotope-overlay">
+                                        <a href="#">
+                                            Artwork #9
+                                        </a>
+                                    </div><!-- /.isotope-overlay -->
+                                </div><!-- /.item -->
+                                <div class="item">
+                                    <img src="assets/images/creations/10.png" alt="artwork image"/>
+                                    <div class="isotope-overlay">
+                                        <a href="#">
+                                            Artwork #10
+                                        </a>
+                                    </div><!-- /.isotope-overlay -->
+                                </div><!-- /.item -->
+                            </div><!-- /.col -->
+                            <div class="col-sm-4">
+                                <div class="item">
+                                    <img src="assets/images/creations/11.png" alt="artwork image"/>
+                                    <div class="isotope-overlay">
+                                        <a href="#">
+                                            Artwork #11
+                                        </a>
+                                    </div><!-- /.isotope-overlay -->
+                                </div><!-- /.item -->
+                                <div class="item">
+                                    <img src="assets/images/creations/12.png" alt="artwork image"/>
+                                    <div class="isotope-overlay">
+                                        <a href="#">
+                                            Artwork #12
+                                        </a>
+                                    </div><!-- /.isotope-overlay -->
+                                </div><!-- /.item -->
+                                <div class="item">
+                                    <img src="assets/images/creations/13.png" alt="artwork image"/>
+                                    <div class="isotope-overlay">
+                                        <a href="#">
+                                            Artwork #13
+                                        </a>
+                                    </div><!-- /.isotope-overlay -->
+                                </div><!-- /.item -->
+                                <div class="item">
+                                    <img src="assets/images/creations/14.png" alt="artwork image"/>
+                                    <div class="isotope-overlay">
+                                        <a href="#">
+                                            Artwork #14
+                                        </a>
+                                    </div><!-- /.isotope-overlay -->
+                                </div><!-- /.item -->
+                                <div class="item">
+                                    <img src="assets/images/creations/15.png" alt="artwork image"/>
+                                    <div class="isotope-overlay">
+                                        <a href="#">
+                                            Artwork #15
+                                        </a>
+                                    </div><!-- /.isotope-overlay -->
+                                </div><!-- /.item -->
+                            </div><!-- /.col -->
+                        </div><!-- /.row -->
+                    </div><!--/.isotope-->
+                </div><!--/.gallery-content-->
+            </div><!--/.container-->
+        </div><!--/.portfolio-details-->
+
+    </section><!--/.portfolio-->
+    <!--artworks end -->
+
+    <!--footer-copyright start-->
+    <footer id="footer-copyright" class="footer-copyright">
+        <div class="container">
+            <div class="hm-footer-copyright text-center">
+                <p>
+                    &copy; copyright Papii ❤. Designed and Developed by Kenth Josh Oyao. Content by Remelyn Estanque
+                </p><!--/p-->
+            </div><!--/.text-center-->
+        </div><!--/.container-->
+
+        <div id="scroll-Top">
+            <div class="return-to-top">
+                <i class="fa fa-angle-up " id="scroll-top" ></i>
+            </div>
+
+        </div><!--/.scroll-Top-->
+
+    </footer><!--/.footer-copyright-->
+    <!--footer-copyright end-->
+
+    <!-- Include all js compiled plugins (below), or include individual files as needed -->
+
+    <script src="assets/js/jquery.js"></script>
+
+    <!--modernizr.min.js-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+
+    <!--bootstrap.min.js-->
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- bootsnav js -->
+    <script src="assets/js/bootsnav.js"></script>
+
+    <!-- jquery.sticky.js -->
+    <script src="assets/js/jquery.sticky.js"></script>
+
+    <!-- for progress bar start-->
+
+    <!-- progressbar js -->
+    <script src="assets/js/progressbar.js"></script>
+
+    <!-- appear js -->
+    <script src="assets/js/jquery.appear.js"></script>
+
+    <!-- for progress bar end -->
+
+    <!--owl.carousel.js-->
+    <script src="assets/js/owl.carousel.min.js"></script>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+
+
+    <!--Custom JS-->
+    <script src="assets/js/custom.js"></script>
+
+</body>
+</html>
